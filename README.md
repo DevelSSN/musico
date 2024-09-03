8e4075b9f618a19c59c5ee9ecc9662da12ee9227
Shashank Shantharam Nayak <shashankshantharamnayak@gmail.com>
2024-09-03 21:17:13 +0530

    Bump Flutter Version; Create Skeleton; Update README

--- README.md
+++ README.md
@@ -7,9 +7,19 @@
 ## Getting Started
 
 Download the app for:
-[Android 13+](/musico/build/)
-[Linux](/musico/build/)
+[Android 13+](blob/musico/build/)
+[Linux](blob/musico/build/)
 
+**Additional Doc to be generated**
+
+## Broad Features
+- [] Local media player
+- [] Playlist support
+- [] iOS app port
+- [] Creation of playlists
+- [] Spotify charting
+- [] Persistence across platforms(Accounts)
+- [] General Streamer
 For help getting started with Flutter development, view the
 [online documentation](https://docs.flutter.dev/), which offers tutorials,
 samples, guidance on mobile development, and a full API reference.
--- lib/player.dart
+++ lib/player.dart
@@ -3,14 +3,18 @@ import 'package:just_audio/just_audio.dart';
 
 class Player extends StatefulWidget {
   const Player({super.key});
-
   @override
   State<Player> createState() => _PlayerState();
 }
 
 class _PlayerState extends State<Player> {
+  final String s = "HI";
+  late AudioHandler _audioHandler;
   @override
   Widget build(BuildContext context) {
+    _audioHandler = AudioHandler();
+    _audioHandler.setURL("Hi");
+    _audioHandler.play();
     return MaterialApp(
         // routes: {
         //   '/': (context) => Loader(),
@@ -22,10 +26,16 @@ class _PlayerState extends State<Player> {
               'Musico',
             ),
           ),
-          body: const Row(children: [
-            Icon(Icons.skip_previous),
-            Icon(Icons.play_arrow),
-            Icon(Icons.arrow_forward)
+          body: Row(children: [
+            IconButton(
+                icon: const Icon(Icons.skip_previous),
+                onPressed: () => setState(_audioHandler.setURL(s))),
+            IconButton(
+                icon: const Icon(Icons.play_arrow),
+                onPressed: () => setState(_audioHandler.setURL(s))),
+            IconButton(
+                icon: const Icon(Icons.skip_next),
+                onPressed: () => setState(_audioHandler.setURL(s))),
           ]),
         ));
   }
@@ -36,10 +46,14 @@ Widget loader() {
 }
 
 class AudioHandler {
+  late var _player;
   AudioHandler() {
-    final _player = AudioPlayer();
-    Future<void> play() => _player.play();
-    Future<void> pause() => _player.pause();
-    Future<void> stop() => _player.stop();
+    _player = AudioPlayer();
+  }
+  Future<void> play() => _player.play();
+  Future<void> pause() => _player.pause();
+  Future<void> stop() => _player.stop();
+  setURL(String s) {
+    _player.setUrl(s);
   }
 }
